f33ce240dab0422996afffa9cb56dcdf0f6691b3
Dennis Castleberry <dcastl2@tigers.lsu.edu>
2016-09-13 12:51:16 -0500

    Corrected mistake.

--- notes/proc/fork.c
+++ notes/proc/fork.c
@@ -10,13 +10,13 @@ int main() {
   if (p_id < 0) {
     printf("Error forking\n");
   } 
-  // If the PID is positive, then this process is the child
+  // If the PID is positive, then this process is the parent
   // process.
   else if (p_id > 0) {
     printf("[%d] Child process\n", p_id);
     execlp("/bin/ls", "ls", NULL);
   } 
-  // Otherwise the PID is 0, then this is the parent process.
+  // Otherwise the PID is 0, then this is the child process.
   // It should wait for the child to finish executing.
   else {
     wait(NULL);
